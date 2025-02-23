e3111a09291b62ab3e29b5928edf08bb4e97da41
roblesdotdev <httpsnip@gmail.com>
2025-02-23 15:24:08 -0300

    add sitemap & generate robots.txt

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,19 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import sitemap from '@astrojs/sitemap'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:3000',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   server: {
     port: 3000,
   },
+
+  integrations: [sitemap()],
 })
