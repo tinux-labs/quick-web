4f6ea6c45f0aecab801f6ab69afc90d180bcd259
roblesdotdev <httpsnip@gmail.com>
2025-02-23 17:11:52 -0300

    support svg sprites with vite plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,8 @@
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
 // @ts-check
 import { defineConfig } from 'astro/config'
-
-import tailwindcss from '@tailwindcss/vite'
-
-import sitemap from '@astrojs/sitemap'
+import { iconsSpritesheet } from 'vite-plugin-icons-spritesheet'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +14,18 @@ export default defineConfig({
   },
 
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      iconsSpritesheet({
+        withTypes: true,
+        typesOutputFile: 'src/components/ui/icons/icon-name.d.ts',
+        inputDir: 'resources/icons',
+        outputDir: 'public/icons',
+        fileName: 'sprite.svg',
+        formatter: 'prettier',
+        iconNameTransformer: name => name.toLowerCase(),
+      }),
+    ],
     build: {
       cssMinify: true,
     },
